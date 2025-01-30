2fdf1dd469b26bb050173f6a9f7f900c7e273bdf
harsh-darkoid <irealade13@gmail.com>
2025-01-29 21:54:47 -0500

    Ver3

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <chrono>
+#include <ctime>
+#include <cstdlib>
 
 
 using namespace std;
@@ -29,10 +31,20 @@ void mergeSort(Flights flight[], int start, int stop,int lineNum);
 
 void Merge(Flights flight[], int start, int mid, int stop);
 
-
-
-
 void merge(int *a, int lineNum);
+
+void quickSort(Flights flight[], int linenum);
+
+void quickSort(Flights flight[], int start, int stop);
+
+int partition(Flights flight[], int start, int stop, int pivot);
+
+int RandomNumFunc(int min, int max);
+
+int BinSearch(Flights flight[], string search, int lineNum);
+
+
+
 
 
 /*
@@ -146,6 +158,10 @@ void selectionSort(Flights flight[], int lineNum){
             }
         }
         swap(flight[i].flightName, flight[min].flightName);
+        swap(flight[i].flightCost, flight[min].flightCost);
+        swap(flight[i].startLocation, flight[min].startLocation);
+        swap(flight[i].endLocation, flight[min].endLocation);
+
     }
 }
 
@@ -158,8 +174,6 @@ void mergeSort(Flights flight[], int start, int stop){
     mergeSort(flight, start, mid);
     mergeSort(flight, mid+1, stop);
     Merge(flight, start, mid,  stop);
-
-
 }
 
 void mergeSort(Flights flight[], int lineNum){
@@ -168,37 +182,109 @@ void mergeSort(Flights flight[], int lineNum){
 }
 
 
-void Merge(Flights flight[], int start, int mid, int stop){
+void Merge(Flights flight[], int start, int mid, int stop) {
     int leftSize = mid - start + 1;
     int rightSize = stop - mid;
 
-    string *leftInfo = new string [leftSize + 1];
-    string *rightInfo = new string[rightSize +1];
-    int i = start;
+    Flights* leftInfo = new Flights[leftSize];
+    Flights* rightInfo = new Flights[rightSize];
 
-    for (; i <= mid; i++) {
-        leftInfo[i-start] = flight[i].flightName;
+    // Copy data to leftInfo
+    for (int i = 0; i < leftSize; i++) {
+        leftInfo[i] = flight[start + i];
     }
-    leftInfo[i-start] =  "ZZZZZZ";
-    for (int j = 0; j <= stop-mid; j++) {
-        rightInfo[j] = flight[mid+1+j].flightName;
+
+    // Copy data to rightInfo
+    for (int j = 0; j < rightSize; j++) {
+        rightInfo[j] = flight[mid + 1 + j];
     }
-    rightInfo[stop-mid] = "ZZZZZZ";
-    int Lt = 0;
-    int Rt = 0;
-    for (int k = start; k <= stop; k++) {
-        if (leftInfo[Lt]<= rightInfo[Rt]) {
-            flight[k].flightName = leftInfo[Lt];
+
+    int Lt = 0; // Index for leftInfo
+    int Rt = 0; // Index for rightInfo
+    int k = start; // Index for the original flight array
+
+    // Merge the temporary arrays back into the original flight array
+    while (Lt < leftSize && Rt < rightSize) {
+        if (leftInfo[Lt].flightName <= rightInfo[Rt].flightName) {
+            flight[k] = leftInfo[Lt];
             Lt++;
         } else {
-            flight[k].flightName= rightInfo[Rt];
+            flight[k] = rightInfo[Rt];
             Rt++;
+        }
+        k++;
+    }
+
+    // Copy any remaining elements from leftInfo
+    while (Lt < leftSize) {
+        flight[k] = leftInfo[Lt];
+        Lt++;
+        k++;
+    }
+
+    // Copy any remaining elements from rightInfo
+    while (Rt < rightSize) {
+        flight[k] = rightInfo[Rt];
+        Rt++;
+        k++;
+    }
+
+    // Clean up memory
+    delete[] leftInfo;
+    delete[] rightInfo;
+}
+
+
+int RandomNumFunc(int min, int max) {
+    return rand() % (max -min + 1) + min;
+
+}
+
+int partition(Flights flight[], int start, int stop, int pivot) {
+    swap(flight[pivot].flightName, flight[stop].flightName);
+    int i = start - 1;
+
+    for (int j = start; j < stop; j++) {
+        if (flight[j].flightName < flight[stop].flightName) {
+            i++;
+            swap(flight[j].flightName,flight[i].flightName);
         }
     }
 
-    delete[] leftInfo;
-    delete[] rightInfo;
+    swap(flight[i + 1].flightName, flight[stop].flightName);
+    return i+1;
+}
 
+
+
+void quickSort(Flights flight[], int start, int stop) {
+    if (start >= stop) {
+        return;
+    }
+    int pivot = partition(flight, start, stop, RandomNumFunc(start, stop));
+    quickSort(flight, start, pivot-1);
+    quickSort(flight, pivot+1, stop);
+}
+
+
+void quickSort(Flights flight[], int lineNum) {
+    quickSort(flight, 0,lineNum-1);
+}
+
+int BinSearch(Flights flight[], string search, int lineNum) {
+   int found = 0;
+
+    for (int i = 0; i < lineNum; i++) {
+        if (flight[i].flightName == search) {
+            cout<<flight[i].flightName<<endl;
+            return found = i;
+        }
+
+
+
+    }
+
+    return found;
 
 }
 
@@ -206,27 +292,29 @@ void Merge(Flights flight[], int start, int mid, int stop){
 
 
 
+
 int main() {
+    srand(time(0));
     string filename = "Flights10_Winter2025.dat";
     const int counter = countLineNum();
     Flights *flight = new Flights[counter];
-
-
-
-
-
-
-    cout<< "---------------------- Question 1 ----------------------"<< endl;
     int lineNum = 0;
-//    readStoredfile(filename,flight, lineNum);
-//    printFlightDetails(flight, counter);
-//
-//
-//    clock_t start = clock();
-//    selectionSort(flight, lineNum);
-//    clock_t total = clock () - start;
-//    cout <<" SELECTION sort took " << total / CLOCKS_PER_SEC << "s" << endl;
-//    printFlightDetails(flight, counter);
+
+
+
+
+
+
+    //cout<< "---------------------- Question 1 ----------------------"<< endl;
+    // readStoredfile(filename,flight, lineNum);
+    // printFlightDetails(flight, counter);
+    //
+    //
+    // clock_t start = clock();
+    // selectionSort(flight, lineNum);
+    // clock_t total = clock () - start;
+    // cout <<" SELECTION sort took " << total / CLOCKS_PER_SEC << "s" << endl;
+    // printFlightDetails(flight, counter);
 //
 //
 //    start = clock();
@@ -239,15 +327,50 @@ int main() {
 
 
 
+
+
     cout<< "---------------------- Question 2 ----------------------"<< endl;
 
 
     readStoredfile(filename,flight, lineNum);
     printFlightDetails(flight, counter);
-
     mergeSort(flight, lineNum);
     printFlightDetails(flight, counter);
-    
+
+    // cout<< "---------------------- Question 3----------------------"<< endl;
+    // readStoredfile(filename,flight, lineNum);
+    // printFlightDetails(flight, counter);
+    // quickSort(flight, lineNum);
+    // printFlightDetails(flight, counter);
+
+    //cout<< "---------------------- Question 4----------------------"<< endl;
+
+    cout<< "---------------------- Question 5----------------------"<< endl;
+
+    // readStoredfile(filename,flight, lineNum);
+    // printFlightDetails(flight, counter);
+    // quickSort(flight, lineNum);
+    // printFlightDetails(flight, counter);
+
+
+    // string search[3] = {"US0842", "FL0044", "GH2333"};
+    //
+    // int result[3];
+    // result[0]= BinSearch(flight, "US0842", lineNum);
+    // cout << result[0];
+
+     // for (int i = 0; i < 3; i++) {
+     //     result[i] = BinSearch(flight, search[i], lineNum);
+     //     if (result[i] == 0) {
+     //         cout<< "The flight "<< search[i] << " does not exist!" <<endl;
+     //     }
+     //     else {
+     //         cout<< "The flight "<< search[i] << " leaves" << flight[result[i]].startLocation << endl;
+     //     }
+     //
+     // }
+
+
 
 
 
